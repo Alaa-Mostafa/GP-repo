2e525144b76700d561aabd7c3dbb01c41738be9c
Alaa Mostafa <alaa.m.mostafa.kamel@gmail.com>
2023-04-27 13:02:03 +0200

    tracks and courses linked with back

--- src/app/Components/tracks-details/tracks-details.component.ts
+++ src/app/Components/tracks-details/tracks-details.component.ts
@@ -1,5 +1,7 @@
 import { Component,OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { ThemeService } from 'src/app/Services/themes.service';
+import { TrackService } from 'src/app/Services/track.service';
 
 @Component({
   selector: 'app-tracks-details',
@@ -10,14 +12,39 @@ export class TracksDetailsComponent implements OnInit {
 
   theme:any;
   background_img_url = "";
-  constructor(private theme_service: ThemeService){}
+
+  track_id = "";
+  track:any = {};
+  track_img = "";
+  courses_num:any = 0; 
+
+  constructor(private theme_service: ThemeService, private myService: TrackService, activated: ActivatedRoute){
+    this.track_id = activated.snapshot.params["id"];
+  }
 
   ngOnInit(): void {
+
+    this.myService.get_track_by_id(this.track_id).subscribe({
+      next: (data) => {
+        this.track = data;
+        this.courses_num = this.track.courses.length;   
+        this.set_track_theme_image();
+      },
+      error: (err) => {
+        alert(err);
+      }
+    });
+
     this.theme_service.get_theme().subscribe({
       next:(data) => {
         this.theme = data;
         this.background_img_url = "assets/themes/" + this.theme['name'] + "/background.jpg";
+        this.set_track_theme_image();
       }
     });
+  }
+
+  set_track_theme_image(){
+    this.track_img  = this.track.track_Img.find((img:any) => {return img.includes(this.theme.name)});
   }
 }
